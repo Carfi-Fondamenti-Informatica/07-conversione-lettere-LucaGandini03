04cb52c7170cb4a96d4e1244abbb1b5c4de07451
LucaGandini03 <116078651+LucaGandini03@users.noreply.github.com>
2022-12-08 23:14:54 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
 int main(){
+  char c;
+  char v;
+  cin>>c;
+  if(checkimput(c)){
+    v=conversione(c);
+    cout<<v;
+  }else{
+    cout<<"errore"<<endl;
+  }
   return 0;
+ 
 }
